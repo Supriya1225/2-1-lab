ea32f0fe8385131603ef12430542d9888abf39c9
Supriya1225 <96834714+Supriya1225@users.noreply.github.com>
2022-02-07 13:57:36 +0530

    console i/o operations
    console i/o operations and output

---
+++ c++lab/experiment 14(a)/main (20).cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+	char x; 
+	// used to scan a single char 
+cout<<"Enter the character:";
+	cin.get(x); 
+	// used to put a single char onto the screen. 
+cout << "The given Character is:";	
+cout.put(x); 
+// ncount character from array 
+	cout.write("\nWelcome to Hyderabad", 10); 
+
+}
+
+
